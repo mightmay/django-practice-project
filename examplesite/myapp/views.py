de371ae42ca2ecf074b50b9584d22e5620276fd8
Daniel Lertsirikarn <mightlert@hotmail.com>
2019-02-13 02:30:39 -0500

    changes

--- examplesite/myapp/views.py
+++ examplesite/myapp/views.py
@@ -6,29 +6,45 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.serializers import ModelSerializer
+
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import GenericAPIView,RetrieveUpdateAPIView,ListCreateAPIView
+from rest_framework.generics import GenericAPIView,RetrieveUpdateDestroyAPIView,ListCreateAPIView
+from django.shortcuts import get_object_or_404, render
 
 
 class StudentsListView(ListCreateAPIView):
     queryset = StudentsModel.objects.all()
     serializer_class = StudentsSerializer
-    
+    def perform_create(self, serializer):
+        data = self.request.data
+        data
+
+        # make some changes to self.request here
+
+        serializer.save(
+                #change some things here
+                field='some new value'
+            )
+
+
+        
+        
 class SchoolsListView(ListCreateAPIView):
     queryset = SchoolsModel.objects.all()
     serializer_class = SchoolsSerializer
     
-class StudentDetailView(RetrieveUpdateAPIView):
+class StudentDetailView(RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a snippet instance.
+    Retrieve, update or delete.
     """
     queryset = StudentsModel.objects.all()
     serializer_class = StudentsSerializer
     
-class SchoolDetailsView(RetrieveUpdateAPIView):
+class SchoolDetailsView(RetrieveUpdateDestroyAPIView):
     queryset = SchoolsModel.objects.all()
     serializer_class = SchoolsSerializer
     
